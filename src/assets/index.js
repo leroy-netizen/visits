900f9481e22302085fdb16ae92153054c0d33dd3
leroy-netizen <leroythomas051@gmail.com>
2022-06-21 15:25:47 +0300

    fetch data | connect API

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,16 @@
-alert("sijakula siku tatu");
+// alert("sijakula siku tatu");
+// Fetch data function
+
+const fetchData = async () => {
+  const response = await fetch(
+    "https://api.musement.com/api/v3/venues/164/activities?limit=6&offset=0"
+  );
+  const data = await response.json();
+  console.log(data);
+};
+
+// fetch("https://api.musement.com/api/v3/venues/164/activities?limit=6&offset=0")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+// console.log(data);
+fetchData();
